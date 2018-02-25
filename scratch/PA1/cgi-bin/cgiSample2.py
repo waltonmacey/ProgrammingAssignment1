44559dbeef9d2be0b859e4f61a1f435b6e465ca7
wmacey <wmacey@vols.utk.edu>
2018-02-25 14:21:15 -0500

    additional examples and form input extra credit

---
+++ scratch/PA1/cgi-bin/cgiSample2.py
@@ -0,0 +1,17 @@
+import cgi
+import cgitb
+cgitb.enable()
+
+print "Content-type: text/html"
+print
+
+print """
+<html>
+
+<head><title>Sample CGI Script</title></head>
+
+<body>
+	<h3> Sample CGI Script </h3>
+</body>
+</html>
+"""
